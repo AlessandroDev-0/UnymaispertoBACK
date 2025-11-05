6284e0c032531d6d9ae19bc5ed990c5bdb027357
Felipe <felipeferreira405060@gmail.com>
2025-11-05 17:07:07 -0300

    .

--- src/js/script.js
+++ src/js/script.js
@@ -47,7 +47,6 @@ const btnLogin = document.getElementById('btn-login');
 if (btnLogin) {
     btnLogin.addEventListener('click', function(event) {
         event.preventDefault();
-        
         window.location.href = '/index.html';
     });
 }
